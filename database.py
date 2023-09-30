7fc5a27b0c3b5f82942fc311c9e40f34d0c0ec27
Garcia Huanaco, Juan <juan.g.huanaco@gmail.com>
2023-09-29 23:46:52 -0600

    production DB change

--- database.py
+++ database.py
@@ -2,11 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://jhcgbzng:h7xuNWTMYARiyMfG47Uy7OfjIbnomtd-@peanut.db.elephantsql.com/jhcgbzng"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
